85d8c046f67a0f0612b6fd2f0cbb95432c179be0
qianchun <qianchun@huli.com>
2018-08-22 12:57:23 +0800

    init

--- src/main/java/com/wind/auth/Application.java
+++ src/main/java/com/wind/auth/Application.java
@@ -4,9 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@MapperScan("com.wind.auth.mapper")
+
+@PropertySource("classpath:dubbo.properties")
+@MapperScan(basePackages = {"com.wind.auth.mapper"})
 @ImportResource(locations={"classpath:spring/applicationContext-*.xml"})
 public class Application {
 	public static void main(String[] args) {
